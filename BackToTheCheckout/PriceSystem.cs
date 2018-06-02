bb48391a144082ee7d088cd5bf13d15ad646007e
Vincent Giang <VinnyGiang@outlook.com>
2018-06-02 20:25:00 +0100

    added pricing and discounts system

---
+++ BackToTheCheckout/PriceSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackToTheCheckout
+{
+    public class PriceSystem
+    {
+        private Dictionary<int, int> itemPrices;
+        private Dictionary<int, Func<int, int>> discountRules;
+
+        public PriceSystem(Dictionary<int, int> itemPrices, Dictionary<int, Func<int, int>> discountRules)
+        {
+            this.itemPrices = itemPrices;
+            this.discountRules = discountRules;
+        }
+
+        public int GetPrice(int itemId)
+        {
+            return itemPrices[itemId];
+        }
+
+        public int CalculateTotalDiscount(BasketItem item)
+        {
+            return discountRules[item.Id].Invoke(item.Quantity);
+        }
+    }
+}
---
+++ UnitTestsCore/PriceSystemTests.cs
@@ -0,0 +1,83 @@
+﻿using BackToTheCheckout;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestsCore
+{
+    [TestClass]
+    public class PriceSystemTests
+    {
+        private Dictionary<int, Func<int, int>> discountRules;
+        private Dictionary<int, int> itemPrices;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            itemPrices = new Dictionary<int, int>();
+            itemPrices.Add(0, 10);
+            itemPrices.Add(1, 5);
+            itemPrices.Add(2, 20);
+
+            discountRules = new Dictionary<int, Func<int, int>>();
+            discountRules.Add(0, (int quantity) => {
+                var timesToApplyDiscount = quantity / 3;
+                var totalDiscountAmount = 20 * timesToApplyDiscount;
+                return totalDiscountAmount;
+            });
+            discountRules.Add(1, (int quantity) => {
+                var timesToApplyDiscount = quantity / 2;
+                var totalDiscountAmount = 5 * timesToApplyDiscount;
+                return totalDiscountAmount;
+            });
+            discountRules.Add(2, (int quantity) => {
+                var timesToApplyDiscount = quantity / 3;
+                var totalDiscountAmount = 10 * timesToApplyDiscount;
+                return totalDiscountAmount;
+            });
+        }
+
+        [TestMethod]
+        public void Should_ReturnCorrectPriceForItem()
+        {
+            // Arrange
+            var item = new BasketItem(0, 1);
+
+            // Act
+            var result = itemPrices[item.Id];
+
+            // Assert
+            Assert.AreEqual(10, result);
+        }
+
+        [TestMethod]
+        public void Should_ReturnCorrectDiscountForItem()
+        {
+            // Arrange
+            var item = new BasketItem(0, 3);
+
+            var priceSystem = new PriceSystem(itemPrices, discountRules);
+            // Act
+            var totalDiscountAmount = priceSystem.CalculateTotalDiscount(item);
+
+            // Assert
+            Assert.AreEqual(20, totalDiscountAmount);
+        }
+
+        [TestMethod]
+        public void Should_ReturnCorrectDiscountTotal_When_DiscountAppliesMoreThanOnce()
+        {
+            // Arrange
+            var item = new BasketItem(0, 6);
+
+            var priceSystem = new PriceSystem(itemPrices, discountRules);
+
+            // Act
+            var totalDiscountAmount = priceSystem.CalculateTotalDiscount(item);
+
+            // Assert
+            Assert.AreEqual(40, totalDiscountAmount);
+        }
+    }
+}
