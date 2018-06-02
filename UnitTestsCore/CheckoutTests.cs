349d2ec592901c3606f215b06ac65612c867d212
Vincent Giang <VinnyGiang@outlook.com>
2018-06-02 23:32:22 +0100

    removed redundant mock class and redundant code

--- UnitTestsCore/CheckoutTests.cs
+++ UnitTestsCore/CheckoutTests.cs
@@ -18,7 +18,7 @@ namespace UnitTestsCore
         public void Should_CorrectlyScanBasketItems()
         {
             // Arrange
-            var item = new ProductItem { Id = 0, Price = 10 };
+            var item = new ProductItem { Id = 0 };
 
             var checkout = new Checkout(null);
 
@@ -33,10 +33,10 @@ namespace UnitTestsCore
         public void Should_ScanItemList()
         {
             // Arrange
-            var item1 = new ProductItem { Id = 0, Price = 10 };
-            var item2 = new ProductItem { Id = 1, Price = 20 };
-            var item3 = new ProductItem { Id = 2, Price = 5 };
-            var item4 = new ProductItem { Id = 0, Price = 10 };
+            var item1 = new ProductItem { Id = 0 };
+            var item2 = new ProductItem { Id = 1 };
+            var item3 = new ProductItem { Id = 2 };
+            var item4 = new ProductItem { Id = 0 };
 
             var items = new List<ProductItem> { item1, item2, item3, item4 };
 
@@ -53,7 +53,12 @@ namespace UnitTestsCore
         public void Should_ReturnPriceOf0_When_BasketIsEmpty()
         {
             // Arrange
-            var checkout = new Checkout(priceSystem);
+            var priceSystemMock = new Mock<IPriceSystem>();
+            priceSystemMock.Setup(x => x.CalculateTotalDiscount(
+                It.IsAny<int>(), It.IsAny<int>()
+                )).Returns(0);
+
+            var checkout = new Checkout(priceSystemMock.Object);
 
             checkout.Scan(new List<ProductItem>());
             // Act
@@ -67,8 +72,12 @@ namespace UnitTestsCore
         public void Should_CorrectlyCalculatePrice_When_ThereIsOneOfEachItem()
         {
             // Arrange
-            var priceSystemMock = new PriceSystemMock();
-            var checkout = new Checkout(priceSystemMock);
+            var priceSystemMock = new Mock<IPriceSystem>();
+            priceSystemMock.Setup(x => x.CalculateTotalDiscount(
+                It.IsAny<int>(), It.IsAny<int>()
+                )).Returns(0);
+
+            var checkout = new Checkout(priceSystemMock.Object);
 
             var item1 = new ProductItem { Id = 0, Price = 10 };
             var item2 = new ProductItem { Id = 1, Price = 20 };
@@ -89,8 +98,12 @@ namespace UnitTestsCore
         public void Should_CorrectlyCalculatePrice_When_ThereAreMultipleOfEachItem()
         {
             // Arrange
-            var priceSystemMock = new PriceSystemMock();
-            var checkout = new Checkout(priceSystemMock);
+            var priceSystemMock = new Mock<IPriceSystem>();
+            priceSystemMock.Setup(x => x.CalculateTotalDiscount(
+                It.IsAny<int>(), It.IsAny<int>()
+                )).Returns(0);
+
+            var checkout = new Checkout(priceSystemMock.Object);
 
             var item1 = new ProductItem { Id = 0, Price = 10 };
             var item2 = new ProductItem { Id = 1, Price = 20 };
@@ -110,8 +123,12 @@ namespace UnitTestsCore
         public void Should_CorrectlyCalculatePrice_When_DiscountIsApplied()
         {
             // Arrange
-            var priceSystemMock = new PriceSystemMock { TotalDiscount = 20 };
-            var checkout = new Checkout(priceSystemMock);
+            var priceSystemMock = new Mock<IPriceSystem>();
+            priceSystemMock.Setup(x => x.CalculateTotalDiscount(
+                It.IsAny<int>(), It.IsAny<int>()
+                )).Returns(20);
+
+            var checkout = new Checkout(priceSystemMock.Object);
 
             var item = new ProductItem { Id = 0, Price = 10 };
 
@@ -159,16 +176,6 @@ namespace UnitTestsCore
             runningTotalPrice = checkout.CalculateTotalPrice();
             Assert.AreEqual(30, runningTotalPrice);
 
-        }
-
-        private class PriceSystemMock : IPriceSystem
-        {
-            public int TotalDiscount { get; set; }
-
-            public int CalculateTotalDiscount(int itemId, int itemQuantity)
-            {
-                return TotalDiscount;
-            }
         }
     }
 }
